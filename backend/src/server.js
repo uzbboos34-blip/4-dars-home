6d34e373fb94c20bd0b97f8414a175ed0cf35764
Ismingiz <email@example.com>
2026-02-05 14:14:40 +0500

    packege fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,15 @@ import { Server } from "socket.io";
 import JWT from "jsonwebtoken";
 import http from "http";
 import pool from "./database/config.js";
-config()
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+config({
+  path: path.join(__dirname, "../../.env")
+})
 
 const server = express()
 
